75458fa180e0e4eab3c2e2c9241a7cebad0d9f4d
aniket umbc <aniket.umbc@gmail.com>
2018-11-09 17:42:28 +0530

    Chapter 2

---
+++ Exercises 2/Exercises 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool isMale = false;
+            if (isMale)
+            {
+                Console.WriteLine("Male");
+            }
+            else
+            {
+                Console.WriteLine("Female");
+            }
+
+            string fname = "Hello";
+            string lname = "world";
+
+            object FullName = fname + " " + lname;
+
+            string DFullName = (string)FullName;
+
+            Console.WriteLine(DFullName);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercises 2/Swap Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = 10;
+            b = 5;
+
+            Console.WriteLine("The value before swap is  a = " + a +" b = " + b);
+            c = a;
+            a = b;          
+            b = c;
+            Console.WriteLine("The value After swap is  a = " + a + " and b = " + b);
+            Console.WriteLine("    o o.o o   ");
+            Console.WriteLine("  o         o      ");
+            Console.WriteLine("  o         o       ");
+            Console.WriteLine("   o       o         ");
+            Console.WriteLine("     o    o       ");
+            Console.WriteLine("       o         ");
+            Console.ReadKey();
+        }
+    }
+}
