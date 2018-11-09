747080b53ec2a943a668bef95110be56b245bd19
aniket umbc <aniket.umbc@gmail.com>
2018-11-09 16:35:58 +0530

    Chapter 1

---
+++ Chapter 1/Excersies 1/Excersies 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersies_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Fname, Lname;
+
+            Console.WriteLine("Please Enter Name");
+            Console.WriteLine("First Name ");
+            Fname = Console.ReadLine();
+            Console.WriteLine("Last Name ");
+            Lname = Console.ReadLine();
+            Console.WriteLine("The Full Name is " + Fname +" " +Lname);
+
+            Console.WriteLine("The Current Date and Time ");
+            DateTime currentDate = DateTime.Now;
+
+            Console.WriteLine(currentDate);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter 1/Excersies 1/SquareRoot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 12345;
+            Console.WriteLine("Square Root of Number is ")
+            double squareRoot = Math.Sqrt(number);
+            Console.Write(+squareRoot);
+
+            for(int i = 1; i <= 100; i++)
+            {
+                if(i%2==0)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.WriteLine("-" +i);
+                }
+            }
+
+            int age;
+            Console.WriteLine("Enter Your Current Age");
+            age = int.Parse (Console.ReadLine());
+            int nowAge = age + 10;
+            Console.WriteLine("After 10 Year " + nowAge);
+            Console.ReadKey();
+        }
+    }
+}
