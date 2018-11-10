8fbe298f85bbc40aa4b4259e192e26854b2e9acf
aniket umbc <aniket.umbc@gmail.com>
2018-11-10 14:05:20 +0530

    more logic

---
+++ Execersies 3/Execersies 3 -2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execersies_3__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Interger ");
+            int num,a,b,c;
+            //num = int.Parse(Console.ReadLine());
+            //bool result = num % 5 == 0 && num % 7 == 0;             
+            //    Console.WriteLine(result);
+
+            num = int.Parse(Console.ReadLine());
+            a = (num / 100);
+            Console.WriteLine(a);
+            b = (a % 10);       
+            if (b == 7)
+            {
+                Console.WriteLine("The Number *********** " +b);
+            }
+            else
+            {
+                Console.WriteLine("Wrong number");
+            } 
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Execersies 3/Execersies 3-3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execersies_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine ("Please enter 4 Digit number");
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine("number is " + num);
+            //Logic of reverse number
+            int a = num % 10;
+            int b = (num / 10) % 10;
+            int c = (num / 100) % 10;
+            int d = (num / 1000) % 10;
+            Console.WriteLine(+a +" " + b + " " + c + " " + d);
+            Console.WriteLine(a + b+ c + d);
+            Console.ReadKey();
+        }
+    }
+}
