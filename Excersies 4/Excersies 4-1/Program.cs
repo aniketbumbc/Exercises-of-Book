b96a3a4599e948b7f8b1fb102dd0c71b9127ad22
aniket umbc <aniket.umbc@gmail.com>
2018-11-11 16:30:12 +0530

    Exersises 4 with fibonacci series

---
+++ Excersies 4/Excersies 4-1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersies_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            int e = int.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine( "A " +a);
+            }else if (b > c)
+            {
+                Console.WriteLine("B " +b);
+            }
+            else if(c > d)
+            {
+                Console.WriteLine("C " +c);
+            }else if(d > e)
+            {
+                Console.WriteLine("D " + d);
+            }
+            else
+            {
+                Console.WriteLine("E " + e);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Excersies 4/Fibonacci Sequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 1;
+            int sum = 0;
+            int count = 0;
+
+            Console.WriteLine(num1);
+
+            while (count < 100)
+            {
+                sum = num1 + num2;
+                num1 = num2;
+                num2 = sum;
+                Console.WriteLine(num2);
+                count++;
+            }           
+            Console.ReadKey();
+        }
+    }
+}
