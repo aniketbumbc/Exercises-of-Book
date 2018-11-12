a33aa4df73e247535d11a70ec7dc3cad9d3131cf
aniket umbc <aniket.umbc@gmail.com>
2018-11-12 08:30:47 +0530

    Exersises 5

---
+++ Execersies 5/Execersies 5-1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execersies_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Two Numbers A ,B and C");
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            if(a>b && a > c)
+            {
+                if (b > c)
+                {
+                    Console.WriteLine(a + ">" + b + ">" + c);
+                }
+                else
+                {
+                    Console.WriteLine(a + ">" + c + ">" + b);
+                }
+            }
+            else if(b > a && b > c)
+            {
+                if (a > c)
+                {
+                    Console.WriteLine(b + ">" + a + ">" + c);
+                }
+                else
+                {
+                    Console.WriteLine(b + ">" + c + ">" + a);
+                }                
+            }else if(c>a && c > b)
+            {
+                if (a > b)
+                {
+                    Console.WriteLine(c + ">" + a + ">" + b);
+                }
+                else
+                {
+                    Console.WriteLine(c + ">" + b + ">" + a);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Execersies 5/Execersies 5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execersies_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Two Numbers A and B");
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("The value of A and B is  A = " + a +" and B = " + b);
+
+            if (a > b)
+            {
+                Console.WriteLine("The value of A is greater Than B  Swappingggg ");
+                int c;
+                c = a;
+                a = b;
+                b = c;
+                Console.WriteLine("The Value of A and B is " + a + " " + b);
+            }
+            else
+            {
+                Console.WriteLine("The value of A and B is  A = " + a + " and B = " + b +" not wapping  ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
