9453b89c3bd07d341fade217838bb5293570b8f9
aniket umbc <aniket.umbc@gmail.com>
2018-11-13 07:47:15 +0530

    Exersises 5 with switch case

---
+++ Execersies 5/Execersies 5-2 SwitchCase/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execersies_5_2_SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Switch Case Here");
+            Console.Write("Enter Variable type (0 -int, 1 -double, 2-string):");
+            int caseEnter = int.Parse(Console.ReadLine());
+            switch (caseEnter)
+            {
+                case 0:
+                    {
+                        Console.WriteLine("Enter int Variable");
+                        int count = int.Parse(Console.ReadLine());
+                        count++;
+                        Console.WriteLine("Int variable case "+ count);
+                        break;
+                    }
+                case 1:
+                    {
+                        Console.WriteLine("Enter Double Variable");
+                        double doublecount = double.Parse(Console.ReadLine());
+                        doublecount++;
+                        Console.WriteLine("Double variable " + doublecount);
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Enter String Variable");
+                        string stringcount = Console.ReadLine();
+                        Console.WriteLine("String variable String" + stringcount + "*");
+                        break;
+                    }
+                     default:Console.WriteLine("Please Enter Correct number");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Execersies 5/Execersies 5-3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execersies_5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Socre With Bounce");
+            int Score = int.Parse(Console.ReadLine());
+            if(Score ==0 || Score > 9)
+            {
+                Console.Write("Please Enter Correct Score");
+            }else if(Score ==1 || Score <= 3)
+            {
+                Console.Write("Score multilply by 10  = " +Score * 10);
+            }
+            else if (Score == 4 || Score <= 6)
+            {
+                Console.Write("Score multilply by 100  = " + Score * 100);
+            }
+            else if (Score == 7 || Score <= 9)
+            {
+                Console.Write("Score multilply by 10  = " + Score * 1000);
+            }
+            Console.ReadKey();
+        }
+    }
+}
