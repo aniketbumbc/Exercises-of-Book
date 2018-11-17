1c5b7275be643e8715be8cc409f8a362a2802afc
aniket umbc <aniket.umbc@gmail.com>
2018-11-17 08:11:05 +0530

    Excersies 6-6

---
+++ Excersies 6/Excersies Real 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersies_Real_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter The Number N ");
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;          
+            int num2 = 1;
+            int num1 = 0;
+            Console.WriteLine("0");
+            Console.WriteLine("1");
+
+            for (int i = 2; i <= n; i++){                 
+                sum = num1 + num2;
+                Console.WriteLine(sum);
+                num1 = num2;
+                num2 = sum;
+               
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Excersies 6/Excersies Real- 6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersies_Real__6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Fact1 = 1;
+            for (int i = 1; i <= 10; i++)
+            {           
+                Fact1 = Fact1 * i;            
+            }
+            Console.WriteLine(Fact1);
+            int Fact2 = 1;
+            for (int i = 1; i <= 5; i++)
+            {
+                Fact2 = Fact2 * i;
+            }
+            Console.WriteLine(Fact2);
+
+            int result = Fact1 / Fact2;
+            Console.WriteLine("Final Result " +result);
+            Console.ReadKey();
+        }
+    }
+}
