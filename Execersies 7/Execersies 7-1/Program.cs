1594a45b73fbc33020908e08d677d842abcac6ab
aniket umbc <aniket.umbc@gmail.com>
2018-11-21 07:59:27 +0530

    Array Excersies

---
+++ Execersies 7/Execersies 7-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execersies_7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Enter array from console  ");
+            int n = int.Parse(Console.ReadLine());
+            int[] num = new int[n];
+           
+            for(int i = 0; i < num.Length; i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+                
+            }
+            Array.Reverse(num);
+            Console.WriteLine("Print Start Here");
+            for(int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine(num[i]);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Execersies 7/Execersies 7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execersies_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Array Is here");
+            int[] numbers = { 3, 5, 6, 7, 8, 2 };
+            int numLength = numbers.Length;
+            int[] revNumbers = new int[numLength];
+            for(int i = 0; i < numLength; i++)
+            {
+                revNumbers[numLength - i - 1] = numbers[i];
+            }
+
+            for(int i = 0; i < numLength; i++)
+            {
+                Console.WriteLine(revNumbers[i]);
+            }
+            Console.Read();
+        }
+    }
+}
