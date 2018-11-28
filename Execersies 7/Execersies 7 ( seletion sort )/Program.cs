8d3ddc2f0336ed859eeaa530dca8c45f5777e69d
aniket umbc <aniket.umbc@gmail.com>
2018-11-28 07:49:25 +0530

    Selection sort

---
+++ Execersies 7/Execersies 7 ( seletion sort )/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execersies_7___seletion_sort__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Selection Sort");
+            int[] numbers = { 3, 2, 4, 1 };
+            int length = numbers.Length;
+
+            for( int i= 0; i < length - 1; i++)
+            {
+                int min_temp = i;
+                for(int j = i+1; j < length; j++)
+                {
+                    if (numbers[j] < numbers[min_temp])
+                    {
+                        min_temp = j;
+                    }
+
+                    int temp = numbers[min_temp];
+                    numbers[min_temp] = numbers[i];
+                    numbers[i] = temp;
+
+                }               
+            }
+            for (int i = 0; i < length; ++i)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+                
+            Console.ReadLine();
+        }
+    }
+}
