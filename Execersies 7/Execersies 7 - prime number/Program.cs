7db7e8219a82d795bf2fae46170b604b3bee7c22
aniket umbc <aniket.umbc@gmail.com>
2018-11-29 07:54:44 +0530

    Prime Number 1000

---
+++ Execersies 7/Execersies 7 - prime number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execersies_7___prime_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Prime Number Program ");
+
+            bool isPrime = true;
+            Console.WriteLine(" Prime Number ");
+            for(int i= 2; i <= 1000; i++)
+            {
+                for(int j = 2; j <= 100; j++)
+                {
+                    if(i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.Write("\t" + i);
+                }
+                isPrime = true;
+            }
+            Console.ReadLine();
+        }
+    }
+}
