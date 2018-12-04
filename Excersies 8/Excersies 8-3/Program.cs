3208cea3dc145baf346664034ad20b312d0c6a8a
aniket umbc <aniket.umbc@gmail.com>
2018-12-04 07:58:29 +0530

    Array find number

---
+++ Excersies 8/Excersies 8-3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersies_8_3
+{
+    class Program
+    {
+        static int findOccurence(int [] array)
+        {
+            int count = 0;           
+            Array.Sort(array);
+            for(int i =0; i < array.Length - 1; i++)
+            {
+                if (array[i] == array[i + 1])
+                {
+                    count++;
+                }                   
+               
+            }
+
+            return count;
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, 4, 4, 3,4,4,9 };
+           Console.WriteLine( " The count value is " + findOccurence(n));
+            Console.ReadKey();
+        }
+    }
+}
