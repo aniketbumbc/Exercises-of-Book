307048fd673d6474f84dcb8664c06880d956b188
aniket umbc <aniket.umbc@gmail.com>
2018-12-04 07:24:54 +0530

    Method review

---
+++ Excersies 8/Excersies 8-1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersies_8_1
+{
+    class Program
+    {
+        static void printData(string str, int number)
+            {
+            Console.WriteLine(" The value of string is " + str + " Number is " + number);
+            }
+        static void printData(string str)
+        {
+            Console.WriteLine(" This is only string " + str);
+        }
+
+        static void printTringle(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Method Overloading ");
+
+            printData("Hello", 007);
+            printData(" Hello No 007 ");
+
+            Console.Write(" See this Tringle and Enter N :- ");
+            
+            int n = int.Parse(Console.ReadLine());
+            printTringle(n);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Excersies 8/Excersies 8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersies_8
+{
+    class Program
+    {
+        static int CalSum(params int [] elements)
+        {
+            int sum = 0;
+            foreach(int element in elements)
+            {
+                sum += element;
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+                      
+            int sum = CalSum(2,4);
+            Console.WriteLine(sum);
+
+            int sum1 = CalSum(5,4,10,34,100);
+            Console.WriteLine(sum1);
+
+            int sum2 = CalSum(10, 4);
+            Console.WriteLine(sum2);
+            Console.ReadKey();
+        }
+    }
+}
