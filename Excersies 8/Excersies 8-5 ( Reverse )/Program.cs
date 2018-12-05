39944ad80aa0b6e9e39d6f0bc864b9d31d5b2d58
aniket umbc <aniket.umbc@gmail.com>
2018-12-05 07:38:22 +0530

    Reverse Number

---
+++ Excersies 8/Excersies 8-5 ( Reverse )/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersies_8_5___Reverse__
+{
+    class Program
+    {
+        static int getReverse(int number)
+        {
+            int reverse = 0 ;
+           while (number > 0)
+            {
+                int remainder = number % 10;
+                reverse = (reverse * 10) + remainder;
+                number = number / 10;
+            }
+            return reverse;
+           
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Value of N :-  ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(getReverse(n));
+            Console.ReadKey();
+        }
+    }
+}
