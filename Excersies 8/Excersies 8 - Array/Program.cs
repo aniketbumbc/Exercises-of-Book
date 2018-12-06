9266fb4fd08c55768ef35dc12aee4930babf3aa4
aniket umbc <aniket.umbc@gmail.com>
2018-12-06 07:18:34 +0530

    Array big element

---
+++ Excersies 8/Excersies 8 - Array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersies_8___Array
+{
+    class Program
+    {
+        static void largeNumber(params int[] numbers)
+        {
+            int bigNumber = 1;
+            Array.Sort(numbers);
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+             if(numbers[i] > numbers[i + 1])
+                {
+                    bigNumber = numbers[i];
+                }
+                else
+                {
+                    bigNumber = numbers[i + 1];
+                }
+            }
+            Console.Write(" The biggest number in array :- " +bigNumber);
+        }
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 4, 5, 3, 10,456,34,23,676 };
+            largeNumber(num);
+            Console.ReadKey();
+        }
+    }
+}
