e098476baf43e3ccd069dc1a58dacfa50e03ee67
aniket umbc <aniket.umbc@gmail.com>
2018-12-06 06:48:59 +0530

    Factorial

---
+++ Excersies 8/Excersies 8-6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersies_8_6
+{
+    class Program
+    {
+        static void factorial(int n)
+        {           
+            int fact = 1;
+            do
+            {
+                fact = fact * n;                
+                n--;
+            } while (n >= 1);
+
+            Console.WriteLine(" The Factorial is " + fact);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Value of N :-  ");
+            int n = int.Parse(Console.ReadLine());
+            factorial(n);
+            Console.ReadKey();
+        }
+    }
+}
