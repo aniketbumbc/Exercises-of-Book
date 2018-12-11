5b8a109f83c02db36ce99351bda4676a6020aaf4
aniket umbc <aniket.umbc@gmail.com>
2018-12-11 07:01:32 +0530

    Recursion ( Factorial )

---
+++ Excersies 10/Excersies 10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersies_10
+{
+    class Program
+    {
+        static decimal Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Recursion Problem");
+            Console.WriteLine(Factorial(4));
+            Console.ReadKey();
+        }
+    }
+}
