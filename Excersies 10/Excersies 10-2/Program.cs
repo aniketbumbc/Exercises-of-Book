1e3bc12f9e919829d253e0e1a7bc18c685badb8e
aniket umbc <aniket.umbc@gmail.com>
2018-12-12 07:10:30 +0530

    Nested Loop

---
+++ Excersies 10/Excersies 10-2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersies_10_2
+{
+    class Program
+    {
+        static void nestedLoop(int[] number, int index)
+        {
+            if (index >= number.Length)
+            {
+                foreach (int numbers in number)
+                    Console.Write("{0}", numbers);
+                Console.WriteLine();
+            }
+            else
+            {
+                for(int i = 1; i <= number.Length; i++)
+                {
+                    number[index] = i;
+                    nestedLoop(number, index + 1);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Nested Loop ");
+
+            int n;
+            Console.Write("Enter Value of N : -");
+            n= int.Parse(Console.ReadLine());
+            int[] number = new int[n];
+            nestedLoop(number, 0);
+            Console.ReadKey();
+        }
+    }
+}
