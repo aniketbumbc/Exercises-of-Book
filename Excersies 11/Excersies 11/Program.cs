777de01db955905c24ba0a1dc5efd684e60f7278
aniket umbc <aniket.umbc@gmail.com>
2018-12-12 07:49:36 +0530

    Class and Method

---
+++ Excersies 11/Excersies 11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersies_11
+{
+    class Program
+    {
+        public class Student
+        {
+            private int rollNumber;
+            private string name;
+
+            public Student()
+            {
+                this.rollNumber = 134;
+                this.name = "Bunny";
+                Console.WriteLine(this.rollNumber);
+                Console.WriteLine(this.name);
+
+            }
+
+            public Student(int RollNumber, string Name)
+            {
+                this.rollNumber = RollNumber;
+                this.name = Name;
+                Console.WriteLine(" This is from Parameter Constructor ");
+                Console.WriteLine(this.rollNumber +" " + this.name);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            Student s2 = new Student(237,"Aniket");
+
+            Console.ReadKey();
+        }
+    }
+}
