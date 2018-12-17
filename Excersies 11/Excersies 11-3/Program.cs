40cb57347349a89acd4142a9948f69fdb0642d63
aniket umbc <aniket.umbc@gmail.com>
2018-12-17 07:44:08 +0530

    Calculation of Sums

---
+++ Excersies 11/Excersies 11-3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersies_11_3
+{
+    class Program
+    {
+    
+        public static void  CalulateSum(string numbers)
+        {
+            int result = 0;
+            string[] newNumber = numbers.Split(' ');
+            for(int i = 0; i <= newNumber.Length-1; i++)
+            {
+                result += Convert.ToInt32(newNumber[i]);
+            }
+
+            Console.WriteLine("The Final Sum " + result);
+
+        }
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(" Enter Value Of Integer ");
+            string numbers = (Console.ReadLine());
+            CalulateSum(numbers);
+            Console.ReadKey();
+        }
+    }
+}
