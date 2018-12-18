65fb7c86bdd21d962944fdbcb7a9b9461f35dae2
aniket umbc <aniket.umbc@gmail.com>
2018-12-18 08:36:41 +0530

    Exception Handle

---
+++ Exercies 12/Exercies 12/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercies_12
+{
+    class Program
+    {
+       using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Excersies_12_Exception
+    {
+        class Program
+        {
+            public static void Calculation(int num1, int num2)
+            {
+                try
+                {
+                    int result = num1 / num2;
+
+                    Console.WriteLine("{0} / {1} = {2}", num1, num2, result);
+                }
+                catch (DivideByZeroException ex)
+                {
+                    //LogError(ex);
+                    Console.WriteLine("Cannot divide by zero. Please try again.");
+                }
+                catch (InvalidOperationException ex)
+                {
+                    //LogError(ex);
+                    Console.Write("Not a valid number. Please try again.");
+                }
+
+            }
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Please enter two integer");
+                int num1 = int.Parse(Console.ReadLine());
+                int num2 = int.Parse(Console.ReadLine());
+                Calculation(num1, num2);
+
+                Console.ReadKey();
+            }
+        }
+    }
+
+}
+}
