6d7ae8534ec15aa5e30fc71e277f231819e62d16
aniket umbc <aniket.umbc@gmail.com>
2018-12-21 07:23:36 +0530

    Random number catch

---
+++ Exercies 12/Exercies 12-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercies_12_1
+{
+    class Program
+    {
+        public static void calculationSum(int a, int b)
+        {
+            try
+            {
+                int result;
+                result = a + b;
+                Console.WriteLine("Hello Result is " + result);
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Done");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Calculation of sum");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            calculationSum(a,b);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercies 12/Exercies 12-3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercies_12_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(" Random Numbers ");
+                int random = new Random().Next(0, 3);
+                Console.WriteLine("The Random Number is " + random);
+                if(random == 0)
+                {
+                    throw new Exception("Random = 0");
+                }if (random == 1)
+                {
+                    Console.WriteLine("Random = 1");
+                    return;
+                }
+                Console.WriteLine("Random = 2");
+            }
+            finally
+            {
+                Console.WriteLine("Finally execute");
+            }
+            Console.ReadKey();
+        }
+    }
+}
